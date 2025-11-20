dfc937ce452a9c42adb1c3b96564e0351c032213
aalmonte-ux <aalmonte@gbox.adnu.edu.ph>
2025-11-20 21:53:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   const pages = ["Faq.html", "Ethics.html", "article.html"];
   const currentPage = window.location.pathname.split("/").pop();
